ca9e666a575cc5c7a0384da602230eb499f8fa49
GabinHLY <gabinhly@gmail.com>
2025-07-10 09:44:24 +0200

    port 3001

--- backend/index.js
+++ backend/index.js
@@ -155,4 +155,4 @@ app.delete('/admin/terrains/:id', (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`)); 
+app.listen(PORT, '0.0.0.0', () => console.log(`Backend running on http://0.0.0.0:${PORT}`)); 
