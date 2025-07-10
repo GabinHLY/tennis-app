7c29d78be7147744e82be8a752466b26f8ff0d7b
GabinHLY <gabinhly@gmail.com>
2025-07-10 14:53:06 +0200

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['tenko.gabinduboc.fr']
+  },
   plugins: [
     react(),
     // tailwindcss(),
